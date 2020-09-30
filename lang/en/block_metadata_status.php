93c021697e6be36124be510d031340d53b8f716e
Quentin Fombaron <q.fombaron@hotmail.fr>
2020-09-30 02:07:07 +0200

    Add default text with quick doc

--- lang/en/block_metadata_status.php
+++ lang/en/block_metadata_status.php
@@ -20,7 +20,12 @@ $string['config_enable_percentage'] = 'Enable percentage label';
 $string['config_enable_percentage_desc'] = '';
 $string['config_block_content'] = 'Block Content';
 $string['config_block_content_desc'] = '';
-$string['config_block_content_default'] = 'Metadata';
+$string['config_block_content_default'] = '<p dir="ltr"><img src="https://i.imgur.com/cnjYDcH.png" alt="Metadata Status logo" width="300" height="169" class="img-responsive atto_image_button_middle"></p>
+<p dir="ltr"><span style="font-size: 0.9375rem; font-family: apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, sans-serif, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;, &quot;Segoe UI Symbol&quot;;"><strong>Metadata Status</strong> tracks <a href="https://moodle.org/plugins/local_metadata" target="_blank">Metadata</a> plugin metadata.</span></p>
+<p dir="ltr"><span style="font-size: 0.9375rem; font-family: apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, sans-serif, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;, &quot;Segoe UI Symbol&quot;;">Key figures summarize the course metadata.</span></p>
+<p dir="ltr"><span style="font-size: 0.9375rem; font-family: apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, sans-serif, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;, &quot;Segoe UI Symbol&quot;;">An icon indicates whether the module is shared (<strong>1</strong>), a progress bar with a threshold informs if the module metadata is sufficiently filled (<strong>2</strong>) and a percentage indicating the amount of metadata filled (<strong>3</strong>).</span></p>
+<p dir="ltr"><img src="https://i.imgur.com/U44bLfm.gif" alt="Metadata Status examples" width="300" height="169" class="img-responsive atto_image_button_middle"></p>
+<p dir="ltr"><span style="font-size: 0.9375rem; font-family: apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, sans-serif, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;, &quot;Segoe UI Symbol&quot;;">Developed by <img src="https://i.imgur.com/PoZ9dn3.png" alt="Quentin Fombaron" width="20" height="20" class="img-responsive atto_image_button_middle"><a href="mailto:q.fombaron@outlook.fr">Quentin Fombaron</a></span></p>';
 $string['config_header_metadata_tracking'] = 'Metadata tracking';
 $string['config_header_metadata_tracking_desc'] = '<span style="color: #FF0000;">Checkboxes cannot be tracked because it is impossible to determine if they are filled or not.</span>';
 $string['config_header_metadata_customization'] = 'Metadata status customization';
