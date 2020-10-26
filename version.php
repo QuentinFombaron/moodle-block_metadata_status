caffaae82e8b92f1dec7f553cdc95e51a36a4275
Quentin Fombaron <q.fombaron@hotmail.fr>
2020-10-26 21:17:07 +0100

    Fix minimum version

--- version.php
+++ version.php
@@ -3,7 +3,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_metadata_status';
 $plugin->version = 2020100900;
-$plugin->requires = 2020061500;
+$plugin->requires = 2020060900;
 
 $plugin->release = 'v1.0.0';
 $plugin->maturity = MATURITY_STABLE;
